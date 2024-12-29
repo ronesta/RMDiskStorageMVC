80ff0d4d67295fcc829efc3f4a77a8729f207462
ronesta <gvozzzd123@gmail.com>
2024-12-29 14:38:01 +0300

    Project without architecture

---
+++ RMDiskStorageMVC/RMDiskStorageMVC/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  RMDiskStorageMVC
+//
+//  Created by Ибрагим Габибли on 29.12.2024.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+}
